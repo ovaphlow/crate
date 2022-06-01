9327935f7f907449d2e61fff4aebe340432f8abd
ovaphlow <ovaphlow@live.com>
2022-06-01 18:25:32 +0800

    format code

--- koa-ts/src/app.ts
+++ koa-ts/src/app.ts
@@ -13,28 +13,27 @@ app.use(pino());
 app.use(bodyParser({ jsonLimit: "16mb" }));
 
 app.on("error", (err: Error, ctx: Context) => {
-  ctx.log.error(`${ctx.req.method} ${ctx.req.url}`);
-  ctx.log.error(err);
+    ctx.log.error(`${ctx.req.method} ${ctx.req.url}`);
+    ctx.log.error(err);
 });
 
 const router = new Router();
 
 (() => {
-  import("./bulletin/endpoint").then(({ endpointDelete, get, getWithResource, put, post }) => {
-    router.get("/crate-api/bulletin/:uuid/:id", getWithResource);
-    router.put("/crate-api/bulletin/:uuid/:id", put);
-    router.delete("/crate-api/bulletin/:id", endpointDelete)
-    router.get("/crate-api/bulletin", get);
-    router.post("/crate-api/bulletin", post);
-  });
+    import("./bulletin/endpoint").then(({ endpointDelete, get, getWithResourceId, put, post }) => {
+        router.get("/crate-api/koa-ts/bulletin/:uuid/:id", getWithResourceId);
+        router.put("/crate-api/koa-ts/bulletin/:uuid/:id", put);
+        router.delete("/crate-api/koa-ts/bulletin/:id", endpointDelete);
+        router.get("/crate-api/koa-ts/bulletin", get);
+        router.post("/crate-api/koa-ts/bulletin", post);
+    });
 })();
 
 (() => {
-  import("./setting/endpoint").then(({ endpointGet }) => {
-    router.get("/crate-api/setting", endpointGet);
-  });
+    import("./setting/endpoint").then(({ get }) => {
+        router.get("/crate-api/koa-ts/setting", get);
+    });
 })();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
--- koa-ts/src/setting/endpoint.ts
+++ koa-ts/src/setting/endpoint.ts
@@ -3,17 +3,17 @@ import { repositoryFilterByCategoryRefId } from "./repository";
 
 const getString = (v: string) => (v ? v : "");
 
-export const endpointGet = async (ctx: Context) => {
-  const { option } = ctx.request.query;
-  if (getString(option?.toString() || "") === "") {
-    ctx.response.body = "[]";
-  }
-  if (option === "filterBy-category-refId") {
-    const { category, refId } = ctx.request.query;
-    const result = await repositoryFilterByCategoryRefId(
-      category?.toString() || "",
-      refId?.toString() || ""
-    );
-    ctx.response.body = result;
-  }
+export const get = async (ctx: Context) => {
+    const { option } = ctx.request.query;
+    if (getString(option?.toString() || "") === "") {
+        ctx.response.body = "[]";
+    }
+    if (option === "filterBy-category-refId") {
+        const { category, refId } = ctx.request.query;
+        const result = await repositoryFilterByCategoryRefId(
+            category?.toString() || "",
+            refId?.toString() || ""
+        );
+        ctx.response.body = result;
+    }
 };
